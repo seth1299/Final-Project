5a088d92fb4fb091ff06067245a81e219625fbe7
seth1299 <59626391+seth1299@users.noreply.github.com>
2021-04-04 14:38:12 -0400

    Commiting Beta v0.1
    Commiting Beta v0.1

--- Final-Project/Assets/Scripts/Enemy Scripts/TargetController.cs
+++ Final-Project/Assets/Scripts/Enemy Scripts/TargetController.cs
@@ -9,28 +9,19 @@ public class TargetController : MonoBehaviour
     public int health;
     [Tooltip("This is the text that the health will be displayed on.")]
     public TextMeshProUGUI healthText;
-    [Tooltip("This is the Mesh Renderer component attached to the same game object you're putting this script on.")]
-    public Renderer rend;
-    [Tooltip("This is the material that the target will change to when it's 'cured'.")]
-    public Material material;
 
     private bool healed = false;
-
-    void Start()
-    {
-        rend = gameObject.GetComponent<Renderer>();
-    }
     
     void OnCollisionEnter(Collision hit)
     {
         if (hit.gameObject.tag == "Projectile")
         {
+            Debug.Log("Getting hit by projectile");
             if (hit.gameObject.GetComponent<ProjectileController>().type == false && health > 0)
             {
                 Debug.Log("Ouchies!");
                 Destroy (hit.gameObject);
-                StartCoroutine("GetHitBySwordForReal");
-                // Code for the enemy getting hit would go here.
+                StartCoroutine("GetHitByArrow");
             }
             else if (hit.gameObject.tag == "Projectile" && hit.gameObject.GetComponent<ProjectileController>().type == true && health <= 0)
             {
@@ -62,10 +53,22 @@ public class TargetController : MonoBehaviour
             StartCoroutine("GetHitBySwordForReal");
     }
 
+    public IEnumerator GetHitByArrow()
+    {
+        // This subtracts one from the enemy's current health and updates their health text accordingly.
+        health--;
+        
+        UpdateHealthText();
+
+        
+        yield return null;
+    }
+
     public IEnumerator GetHitBySwordForReal()
     {
         // This subtracts one from the enemy's current health and updates their health text accordingly.
         health--;
+
         UpdateHealthText();
 
         for (int i = 0; i < 60; i++)
@@ -74,31 +77,14 @@ public class TargetController : MonoBehaviour
             yield return null;
             yield return null;
         }
+
         yield return null;
     }
 
     public void GetHealed()
     {
         healed = true;
-        rend.material = material;
     }
-
-    /*
-    public IEnumerator GetHealed()
-    {
-        Debug.Log("Healing enemy...");
-        while (gameObject.GetComponent<Renderer>().material.color.a > 0)
-        {
-            Debug.Log(rend.material.color.a);
-            Color color = rend.material.color;
-            float fadeAmount = color.a - (1f * Time.deltaTime);
-            color = new Color(color.r, color.g, color.b, fadeAmount);
-            rend.material.color = color;
-            yield return null;
-        }
-        Debug.Log("Finished healing enemy...");
-    }
-    */
 
     private void UpdateHealthText()
     {
@@ -106,7 +92,7 @@ public class TargetController : MonoBehaviour
             healthText.text = "Health: " + health;
         else if (health <= 0 && !healed)
         {
-            healthText.fontSize = 0.4f;
+            //healthText.fontSize = 0.4f;
             healthText.text = "Now's your chance, heal them with your magical ability!";
         }
         else
--- Final-Project/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Final-Project/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿/* Made by Fisher Hensley for Nine Lives Studio
+﻿/* Made by Fisher Hensley and Seth Grimes for Nine Lives Studio
    and UCF's DIG-4715 Class. Controls player
    movement. */
 using System.Collections;
@@ -9,86 +9,168 @@ using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    [Tooltip("This is the CharacterController component attached to the player character. You can just drag the character onto this.")]
     public CharacterController controller;
+    [Tooltip("This is the speed of the player character, or how fast the character can walk.")]
     public float speed;
+    
+    [HideInInspector]
     public bool aiming;
     public GameObject projectile1;
+
+    [Tooltip("This is the magical curing projectile that the player shoots.")]
     public GameObject projectile2;
+
+    [Tooltip("This is the camera that will be used for the third person view.")]
     public Camera thirdPersonCam;
+
+    [Tooltip("This is the camera that will be used for the first person view.")]
     public Camera firstPersonCam;
-    public Text UI;
+
+    [Tooltip("This is the canvas that the crosshair is on.")]
+    public Canvas crosshairCanvas;
+
+    [Tooltip("This is how long it takes between bow shots.")]
+    public float bowShotCooldown;
+
+    // This is how much time is remaining on the current bow shot cooldown.
+    private float bowShotCooldownTimeRemaining;
+    
+    // This is how much ammo the player has.
     private int ammo;
+
+    // This is how much mana the player has.
     private int mana;
+
+    // This is the maximum amount of mana that the player can reach.
     private int manaMax;
+
+    // This is how long the player must wait before regenerating mana. Negative values are faster, positive values are slower.
     private float regenTimer;
+
+    // This is the move direction of the player.
     private Vector3 moveDirect;
+
+    // This tracks if the game is paused or not.
     private bool isPaused = false;
+
+    public GameObject GameController;
+    private GameController gc;
+
+    void Awake()
+    {
+        gc = GameController.GetComponent<GameController>();
+        crosshairCanvas.enabled = false;
+    }
 
     void Start()
     {
+        regenTimer = gc.GetRegenTimer();
+
+        manaMax = gc.GetManaMax();
+
         Cursor.lockState = CursorLockMode.Locked;
-        ammo = 10;
-        mana = 20;
+
         manaMax = mana;
+
         thirdPersonCam.enabled = true;
+
         firstPersonCam.enabled = false;
+
         aiming = false;
-        regenTimer = 0;
+
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-            isPaused = !isPaused;
-        if (!isPaused)
-        {
-        float x = Input.GetAxis ("Horizontal");
-        float z = Input.GetAxis ("Vertical");
-        transform.Rotate(new Vector3 (0, Input.GetAxis("Mouse X"), 0), Space.Self);
-        moveDirect = transform.right * x + transform.forward * z;
+        if (bowShotCooldown != 0)
+            bowShotCooldownTimeRemaining -= Time.deltaTime;
 
-        UI.text = "Ammo : " + ammo + "\n" + "Mana : " + mana;
+        if (bowShotCooldownTimeRemaining < 0)
+            bowShotCooldownTimeRemaining = 0;
+
+        ammo = gc.GetAmmo();
+
+        mana = gc.GetMana();
+
+        if (Input.GetButtonDown("Pause Keyboard") || Input.GetButtonDown("Pause Controller"))
+            isPaused = !isPaused;
+
+        if (!isPaused)
         
-        if (Input.GetMouseButtonDown(1))
+        {
+
+        float x = Input.GetAxis ("Horizontal");
+
+        float z = Input.GetAxis ("Vertical");
+
+        transform.Rotate(new Vector3 (0, Input.GetAxis("Mouse X"), 0), Space.Self);
+
+        moveDirect = transform.right * x + transform.forward * z;
+        
+        if (Input.GetButtonDown("Right Click") || Input.GetButtonDown("Right Click Controller"))
         {
             CameraSwitch();
         }
 
-        if (Input.GetKeyDown(KeyCode.E) && aiming == true|| Input.GetKeyDown(KeyCode.R) && aiming == true)
+        if ( ( ( Input.GetButtonDown("Ranged Attack Keyboard") || Input.GetButtonDown("Ranged Attack Controller") )  && aiming == true ) || ( ( Input.GetButtonDown("Cure Ability Keyboard") || Input.GetButtonDown("Cure Ability Controller") ) && aiming == true))
         {
-            if (Input.GetKeyDown(KeyCode.E))
+
+            if ( Input.GetButtonDown("Ranged Attack Keyboard") || Input.GetButtonDown("Ranged Attack Controller") && aiming )
             {
-                if (ammo > 0)
+
+                if (ammo > 0 && aiming && bowShotCooldownTimeRemaining == 0)
                 {
-                    GameObject projectileShot = Instantiate(projectile1, transform.position + transform.forward, Quaternion.Euler(new Vector3(0, transform.rotation.x, 0)));
+                    bowShotCooldownTimeRemaining = bowShotCooldown;
+
+                    gc.SetAmmo(-1);
+                    
+                    GameObject projectileShot = Instantiate(projectile1, transform.position + transform.forward, Quaternion.identity);
+                    
+                    projectileShot.transform.Rotate(80f, 180f, 150f);
+                    
                     ProjectileController projectileActive = projectileShot.GetComponent<ProjectileController>();
+                    
                     projectileActive.Launch(gameObject);
-                    ammo--;
                 }
+
             }
-            else 
+
+            else if (aiming)
             {
-                if (mana >= 5)
+
+                if (mana >= 5 && aiming)
                 {
+                    gc.SetMana(-5);
+                    
                     GameObject projectileShot = Instantiate(projectile2, transform.position + transform.forward, Quaternion.Euler(new Vector3(0, transform.rotation.x, 0)));
+                    
                     ProjectileController projectileActive = projectileShot.GetComponent<ProjectileController>();
+                    
                     projectileActive.Launch(gameObject);
-                    mana -= 5;
                 }
+
             }
         }
 
         if (mana < manaMax)
         {
+
             regenTimer += Time.deltaTime;
+
             {
+
                 if (regenTimer > 2.5)
                 {
                     mana++;
                     regenTimer = 0;
                 }
+
             }
+
         }
+
         }
     }
     void FixedUpdate() // Physics is calculated when fixedupdate runs, movement here is consistent between the editor and builds.
@@ -96,18 +178,59 @@ public class PlayerController : MonoBehaviour
         if (!isPaused)
             controller.Move(moveDirect * speed * Time.deltaTime);
     }
+    public bool GetAiming()
+    {
+        return aiming;
+    }
     void CameraSwitch() // This controls what camera the player is using.
     {
         thirdPersonCam.enabled = !thirdPersonCam.enabled;
+
         firstPersonCam.enabled = !firstPersonCam.enabled;
+        
+        if (crosshairCanvas.enabled)
+        {
+            Debug.Log("Disabling crosshair canvas");
+
+            crosshairCanvas.enabled = false;
+        }
+
+        else
+        {
+            crosshairCanvas.enabled = true;
+        }
+
         aiming = !aiming;
     }
     void OnTriggerEnter(Collider entered)
     {
-        if (entered.gameObject.tag == "Ammunition")
+        if (entered == null)
+        {
+            
+        }
+
+        else if (entered.gameObject.tag == "Ammunition")
         {
             ammo++;
             Destroy(entered.gameObject);
         }
+        else if (entered.gameObject.tag == "BasicEnemy")
+        {
+            Debug.Log("Touching enemy");
+
+            gc.SetHealth(-1);
+
+            StartCoroutine("Knockback");
+        }
+    }
+
+    public IEnumerator Knockback()
+    {
+        for (int i = 0; i < 120; i++)
+            {
+                gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.125f);
+                yield return null;
+                yield return null;
+            }
     }
 }
