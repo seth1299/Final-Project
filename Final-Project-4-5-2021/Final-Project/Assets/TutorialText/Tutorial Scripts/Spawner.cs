045327cc8a6473f615afb8b075c076f64d0e51bc
Jess <jwise1596@gmail.com>
2021-04-08 16:00:57 -0400

    Tutorial Added

---
+++ Final-Project-4-5-2021/Final-Project/Assets/TutorialText/Tutorial Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour{
+
+    public GameObject monster;
+    public float startTimeBtwMonster;
+    private float timeBtwMonster;
+    public int numberOfMonsters;
+
+       // Update is called once per frame
+    void Update()
+    {
+        if(timeBtwMonster <= 0 && numberOfMonsters > 0)
+        {
+            Instantiate(monster, transform.position, Quaternion.identity);
+            timeBtwMonster = startTimeBtwMonster;
+            numberOfMonsters--;
+        }
+        else
+        {
+            timeBtwMonster -= Time.deltaTime;
+        }
+        
+    }
+}
---
+++ Final-Project-4-5-2021/Final-Project/Assets/TutorialText/Tutorial Scripts/TutorialManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour{
+public GameObject[] popUps;
+private int popUpIndex;
+public float waitTime =2f;
+
+void Update (){
+
+        for (int i = 0; i < popUps.Length; i++) {
+            if(i == popUpIndex){
+                popUps[i].SetActive(true);
+            } else {
+                popUps[i].SetActive(false);
+            }
+        }
+
+    if(popUpIndex == 0)
+    {
+        if(Input.GetKeyDown(KeyCode.Space))  
+            {
+                popUpIndex++;
+            }
+    } 
+    else if(popUpIndex == 1)
+    {
+        if(Input.GetKeyDown(KeyCode.Space))  
+        {
+            popUpIndex++;
+        }
+    }
+    else if(popUpIndex == 2)
+    {
+        if(Input.GetKeyDown(KeyCode.Space))  
+        {
+            popUpIndex++;
+        }
+    }
+    else if(popUpIndex == 3)
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            popUpIndex++;
+        }
+    }
+    else if(popUpIndex == 4)
+    {
+        if(Input.GetKeyDown(KeyCode.Space))  
+        {
+            popUpIndex++;
+        }
+    }
+else if(popUpIndex == 5)
+    {
+        if(Input.GetKeyDown(KeyCode.Space))  
+        {
+            popUpIndex++;
+        }
+    }
+}
+}
